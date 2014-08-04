978094cb9991cc543f59f6ce830a9a753c726802
Anthony Debruyn <antdebru@ulb.ac.be>
2014-08-04 17:13:11 +0200

    again

--- findit.js
+++ findit.js
@@ -1,23 +1,19 @@
-var onFailSoHard = function(e)
-    {
-            console.log('failed',e);
-    }
-
-window.URL = window.URL || window.webkitURL ;
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-
-var video = document.querySelector('video');
-
-if(navigator.getUserMedia)
-{
-	alert("hey");
-    navigator.getUserMedia({video: true},function(stream) {
-    video.src = window.URL.createObjectURL(stream);
-    },onFailSoHard);
-}
-
-document.getElementById('snapshot').onclick = function() { 
-    var canvas = document.getElementById('canvas'); 
-    var ctx = canvas.getContext('2d'); 
-    ctx.drawImage(video,0,0); 
+if(navigator.webkitGetUserMedia!=null) { 
+    var options = { 
+        video:true, 
+        audio:true 
+    }; 
+     
+    //request webcam access 
+    navigator.webkitGetUserMedia(options, 
+        function(stream) { 
+            //get the video tag 
+            var video = document.querySelector('video'); 
+            //turn the stream into a magic URL 
+            video.src = window.webkitURL.createObjectURL(stream); 
+        }, 
+        function(e) { 
+            console.log("error happened"); 
+        } 
+    ); 
 } 
